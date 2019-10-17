af15b3ee62adfaf5db18256420ea2cc630b0a35a
REX <1395719047@qq.com>
2019-10-17 20:39:43 +0800

    foreach 打印数组

---
+++ arrayLength.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName arrayLength
+ * @Auther REX
+ * @DATE 2019/10/17 20:35
+ * @Version 1.0
+ **/
+public class arrayLength {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5};
+
+        for(int x : array){
+            System.out.print(x + " ");
+        }
+    }
+}
